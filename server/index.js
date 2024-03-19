bbc88124c52eaef2a7f19adedc4410b287f86b69
tahrimm <tahrim124@gmail.com>
2024-03-19 22:41:51 +0600

    Added new function

--- server/index.js
+++ server/index.js
@@ -18,21 +18,21 @@ mongoose
     console.log(`Connected to MongoDB`);
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
    
-    jobdata.jobs.forEach(async (job) => {
-      // Replace photo URLs with your own URLs
-      job.applicants.forEach(applicant => {
-        // Assuming you have 3 photos and you want to rotate them
-        applicant.photo = `C:/Users/Tahrim/Documents/Project/HR dashboard/server/${Math.floor(Math.random() * 3) + 1}.jpg`;
-      });
+    // jobdata.jobs.forEach(async (job) => {
+    //   // Replace photo URLs with your own URLs
+    //   job.applicants.forEach(applicant => {
+    //     // Assuming you have 3 photos and you want to rotate them
+    //     applicant.photo = `C:/Users/Tahrim/Documents/Project/HR dashboard/server/${Math.floor(Math.random() * 3) + 1}.jpg`;
+    //   });
 
-      const newJob = new Job({ job_title: job.job_title, applicants: job.applicants });
-      try {
-        await newJob.save();
-        console.log(`Inserted job: ${job.job_title}`);
-      } catch (error) {
-        console.error(`Error inserting job: ${job.job_title}`, error);
-      }
-    });
+    //   const newJob = new Job({ job_title: job.job_title, applicants: job.applicants });
+    //   try {
+    //     await newJob.save();
+    //     console.log(`Inserted job: ${job.job_title}`);
+    //   } catch (error) {
+    //     console.error(`Error inserting job: ${job.job_title}`, error);
+    //   }
+    // });
   
   })
   .catch((error) => console.error(`${error} did not connect`));
